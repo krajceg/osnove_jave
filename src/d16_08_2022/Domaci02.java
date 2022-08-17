10cba86184d31ea512169bdac22d08dda12f9fbc
Svetolik Kljajic <svetolikkljajic@gmail.com>
2022-08-17 16:35:22 +0200

    Dodatak teksta za Domaci02

--- src/d16_08_2022/Domaci02.java
+++ src/d16_08_2022/Domaci02.java
@@ -3,6 +3,18 @@ package d16_08_2022;
 public class Domaci02 {
 
 	public static void main(String[] args) {
+		
+//		Kreirati klasu SmartAirConditioning koja ima:
+//			atribut za marku klime (npr: Samsung, Galanz, Gree, …)
+//			atribut za izabranu temperaturu 
+//			atribut za mod (hladi/greje) - string
+//			metodu za stampu (proizvoljno)
+//			metodu koja vraca informaciju da li je napolju veca temperatura od one na koju je klima 
+//			postavljena. Metoda kao parametar prima temperaturu koja je napolju.
+//
+//
+//			U glavnom programu kreirati minimum 2 objekta klase SmartAirConditioning i za svaki od njih 
+//			pozvati metode
 
 		Domaci02_SmartAirConditioning a = new Domaci02_SmartAirConditioning();
 		Domaci02_SmartAirConditioning b = new Domaci02_SmartAirConditioning();
